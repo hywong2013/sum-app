6601dd5c26e89b81b08bce077e322bb8daf074e5
Angus_Wong 黃浩然 (MIS) <Angus_Wong@coolermaster.com.tw>
2025-08-22 17:52:48 +0800

    test update

--- cypress/e2e/sum.cy.js
+++ cypress/e2e/sum.cy.js
@@ -25,7 +25,15 @@ describe("加總前端 E2E", () => {
     cy.contains("結果：5.4").should("be.visible");
   });
 
-  it("Test4_輸入無效字元要提示錯誤", () => {
+  it("Test4_成功加總 2.2 + 3.2 = 5.4", () => {
+    cy.visit("http://localhost:5173");
+    cy.get('input[placeholder="輸入第一個數字"]').type("2.2");
+    cy.get('input[placeholder="輸入第二個數字"]').type("3.2");
+    cy.contains("計算").click();
+    cy.contains("結果：5.4").should("be.visible");
+  });
+
+  it("Test5_輸入無效字元要提示錯誤", () => {
     cy.visit("http://localhost:5173");
     cy.get('[data-cy="a"]').type("abc");
     cy.get('[data-cy="b"]').type("3");
